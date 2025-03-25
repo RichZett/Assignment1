41fdfe9794ec69357b2c74a25366ebd06e68f26a
RichZett <rz_13@live.se>
2025-03-25 14:56:06 +0000

    Fixed CMake

---
+++ build/Assignment1Lib/CMakeFiles/Assignment1Lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment1Lib.dir/assignment1.c.o"
+  "CMakeFiles/Assignment1Lib.dir/assignment1.c.o.d"
+  "libAssignment1Lib.a"
+  "libAssignment1Lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment1Lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
